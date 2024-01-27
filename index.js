cd0a52be11f4c2e80b27431d873b5fc48e1fefe6
favor-star <favoureliab@gmail.com>
2024-01-27 10:35:44 +0200

    fixed the a defaults

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const aLinks = document.querySelectorAll("a");
 console.log(aLinks);
 aLinks.forEach((elem) => {
   elem.addEventListener("click", (e) => {
-      e.preventDefault();
+    //   e.preventDefault();
   });
 });
+
+const view3d = document.querySelector(".view__3d");
+console.log(view3d);
+view3d.addEventListener("click", (e) => {
+    e.target.href = "view.html"
+   console.log(e.target);
+})
+
